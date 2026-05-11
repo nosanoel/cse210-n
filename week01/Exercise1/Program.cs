b8857fc6766190308e939e54cff4606138981b59
unknown <idehennosa231@gmail.com>
2026-05-11 04:56:30 +0100

    jjjj

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,12 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("what is your name");
-         string first = Console.ReadLine();
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
 
-         Console.Write("what is your last name");
-         string last = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
 
-         Console.WriteLine($"your name is {last},{first} {last}");
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
